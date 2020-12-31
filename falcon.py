125455639241793a5eb3147fbf71a75d8817de20
Wout Bittremieux <wout@bittremieux.be>
2020-12-30 16:58:28 -0800

    Explicitly import config values

--- falcon.py
+++ falcon.py
@@ -1,5 +1,6 @@
 import copy
 import logging
+import os
 import sys
 
 import joblib
@@ -9,12 +10,12 @@ import pandas as pd
 import scipy.sparse as ss
 from sklearn.utils import murmurhash3_32
 
+import config
 from cluster import cluster, spectrum
-from config import *
 from ms_io import ms_io
 
 
-if __name__ == '__main__':
+def main():
     # Configure logging.
     logging.captureWarnings(True)
     root = logging.getLogger()
@@ -32,29 +33,29 @@ if __name__ == '__main__':
     # Initialize the logger.
     logger = logging.getLogger('spectrum_clustering')
 
-    if os.path.isdir(work_dir):
+    if os.path.isdir(config.work_dir):
         logging.warning('Working directory %s already exists, previous '
-                        'results might get overwritten', work_dir)
+                        'results might get overwritten', config.work_dir)
     else:
-        os.makedirs(work_dir)
+        os.makedirs(config.work_dir)
 
     # Read the spectra from the input files.
-    spectra, spectra_raw = {charge: [] for charge in charges}, {}
-    logger.info('Read spectra from %d peak file(s)', len(filenames))
+    spectra, spectra_raw = {charge: [] for charge in config.charges}, {}
+    logger.info('Read spectra from %d peak file(s)', len(config.filenames))
     # noinspection PyProtectedMember
     for file_spectra in joblib.Parallel(n_jobs=-1)(
             joblib.delayed(ms_io._get_spectra)(filename)
-            for filename in filenames):
+            for filename in config.filenames):
         for spec_raw in file_spectra:
-            spec_raw.identifier = f'mzspec:{pxd}:{spec_raw.identifier}'
+            spec_raw.identifier = f'mzspec:{config.pxd}:{spec_raw.identifier}'
             spectra_raw[spec_raw.identifier] = copy.copy(spec_raw)
             # Discard low-quality spectra.
             spec_processed = spectrum.process_spectrum(
-                spec_raw, min_peaks, min_mz_range, min_mz, max_mz,
-                remove_precursor_tolerance, min_intensity, max_peaks_used,
-                scaling)
+                spec_raw, config.min_peaks, config.min_mz_range, config.min_mz,
+                config.max_mz, config.remove_precursor_tolerance,
+                config.min_intensity, config.max_peaks_used, config.scaling)
             if (spec_processed is not None
-                    and spec_processed.precursor_charge in charges):
+                    and spec_processed.precursor_charge in config.charges):
                 spectra[spec_processed.precursor_charge].append(spec_processed)
     # Make sure the spectra are sorted by precursor m/z for every charge state.
     for charge, spectra_charge in spectra.items():
@@ -62,9 +63,9 @@ if __name__ == '__main__':
             spectra_charge, key=lambda spec: spec.precursor_mz)
 
     # Pre-compute index hash mappings.
-    vec_len, min_mz, max_mz = spectrum.get_dim(min_mz, max_mz,
-                                               fragment_mz_tolerance)
-    hash_lookup = np.asarray([murmurhash3_32(i, 0, True) % hash_len
+    vec_len, min_mz, max_mz = spectrum.get_dim(config.min_mz, config.max_mz,
+                                               config.fragment_mz_tolerance)
+    hash_lookup = np.asarray([murmurhash3_32(i, 0, True) % config.hash_len
                               for i in range(vec_len)], np.uint32)
 
     # Cluster the spectra per charge.
@@ -73,29 +74,32 @@ if __name__ == '__main__':
         logger.info('Cluster %d spectra with precursor charge %d',
                     len(spectra_charge), charge)
         # Convert the spectra to hashed vectors.
-        vectors = np.zeros((len(spectra_charge), hash_len), np.float32)
+        vectors = np.zeros((len(spectra_charge), config.hash_len), np.float32)
         joblib.Parallel(n_jobs=-1, prefer='threads')(
-            joblib.delayed(spectrum.to_vector)(spec, vectors[i, :], min_mz,
-                                               max_mz, fragment_mz_tolerance,
-                                               hash_lookup, False)
+            joblib.delayed(spectrum.to_vector)(
+                spec, vectors[i, :], min_mz, max_mz,
+                config.fragment_mz_tolerance, hash_lookup, False)
             for i, spec in enumerate(spectra_charge))
 
         # Cluster the vectors.
         precursor_mzs = np.asarray([spec.precursor_mz
                                     for spec in spectra_charge])
-        dist_filename = os.path.join(work_dir, f'dist_{charge}.npz')
+        dist_filename = os.path.join(config.work_dir, f'dist_{charge}.npz')
         if not os.path.isfile(dist_filename):
             pairwise_dist_matrix = cluster.compute_pairwise_distances(
-                vectors, precursor_mzs, precursor_tol_mass, precursor_tol_mode,
-                mz_interval, n_neighbors, n_neighbors_ann, precursor_tol_mass,
-                precursor_tol_mode, batch_size, n_probe,
-                os.path.join(work_dir, str(charge)))
+                vectors, precursor_mzs, config.precursor_tol_mass,
+                config.precursor_tol_mode, config.mz_interval,
+                config.n_neighbors, config.n_neighbors_ann,
+                config.precursor_tol_mass, config.precursor_tol_mode,
+                config.batch_size, config.n_probe,
+                os.path.join(config.work_dir, str(charge)))
             ss.save_npz(dist_filename, pairwise_dist_matrix)
         else:
             pairwise_dist_matrix = ss.load_npz(dist_filename)
         clusters = cluster.generate_clusters(
-            pairwise_dist_matrix, eps, min_samples, precursor_mzs,
-            precursor_tol_mass, precursor_tol_mode)
+            pairwise_dist_matrix, config.eps, config.min_samples,
+            precursor_mzs, config.precursor_tol_mass,
+            config.precursor_tol_mode)
         # Make sure that different charges have non-overlapping cluster labels.
         mask_no_noise = clusters != -1
         clusters[mask_no_noise] += current_label
@@ -116,8 +120,13 @@ if __name__ == '__main__':
     # Export cluster memberships and representative spectra.
     clusters_all = (pd.concat(clusters_all, ignore_index=True)
                     .sort_values('identifier', key=natsort.natsort_keygen()))
-    clusters_all.to_csv(os.path.join(work_dir, 'clusters.csv'), index=False)
-    ms_io.write_spectra(os.path.join(work_dir, 'clusters.mgf'),
+    clusters_all.to_csv(os.path.join(config.work_dir, 'clusters.csv'),
+                        index=False)
+    ms_io.write_spectra(os.path.join(config.work_dir, 'clusters.mgf'),
                         sorted(representatives, key=lambda spec: spec.cluster))
 
     logging.shutdown()
+
+
+if __name__ == '__main__':
+    main()
