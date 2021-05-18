4b030d242ec05c891e9e764e1db29207c53bacde
Wout <wout@bittremieux.be>
2021-05-18 09:17:06 -0700

    Fix version retrieval from package name

--- falcon/__init__.py
+++ falcon/__init__.py
@@ -1,14 +1,16 @@
+pkg_name = 'falcon-ms'
+
 try:
     from importlib.metadata import version, PackageNotFoundError
 
     try:
-        __version__ = version(__name__)
+        __version__ = version(pkg_name)
     except PackageNotFoundError:
         pass
 except ImportError:
     from pkg_resources import get_distribution, DistributionNotFound
 
     try:
-        __version__ = get_distribution(__name__).version
+        __version__ = get_distribution(pkg_name).version
     except DistributionNotFound:
         pass
