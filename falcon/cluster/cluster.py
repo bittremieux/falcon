fix empty bins resulting in zero intensities after spectra averaging

falcon/cluster/cluster.py
@@ -710,7 +710,9 @@ def _get_cluster_average(
             )
             del spectra_to_average
             # Outlier rejection
-            bins_peaks = _outlier_rejection(bins_idx, bins_peaks, n_min, n_max)
+            bins_idx, bins_peaks = _outlier_rejection(
+                bins_idx, bins_peaks, n_min, n_max
+            )
             # Average peaks
             bins_peaks = _average_peaks(bins_idx, bins_peaks)
             # Construct average spectrum
@@ -809,14 +811,26 @@ def _outlier_rejection(
     for _ in range(len(bins_indices)):
         cleaned_bins_peaks.append(np.empty(0, np.float32))
 
+    zero_peaks = np.zeros(len(bins_indices), dtype=np.bool_)
     for i in range(len(bins_indices)):
         intensities = bins_peaks[i]
-        if len(intensities) > 1:
-            cleaned_bins_peaks[i] = _sigma_clipping(intensities, n_min, n_max)
+        if len(intensities) > 2:
+            clipped = _sigma_clipping(intensities, n_min, n_max)
+            if len(clipped) < 1:
+                zero_peaks[i] = True
+            else:
+                cleaned_bins_peaks[i] = clipped
         else:
             cleaned_bins_peaks[i] = intensities
 
-    return cleaned_bins_peaks
+    # Remove empty bins
+    bins_indices = bins_indices[~zero_peaks]
+    new_cleaned_bins_peaks = []
+    for i in range(len(cleaned_bins_peaks)):
+        if not zero_peaks[i]:
+            new_cleaned_bins_peaks.append(cleaned_bins_peaks[i])
+
+    return bins_indices, new_cleaned_bins_peaks
 
 
 @nb.njit(cache=True)

falcon/ms_io/mgf_io.py
@@ -2,6 +2,7 @@ import math
 from typing import Dict, IO, Iterable, List, Union
 
 import numba as nb
+import numpy as np
 import pyteomics.mgf
 import spectrum_utils.spectrum as sus
 
@@ -141,6 +142,4 @@ def _scale_intensities(intensity: List[float]) -> List[float]:
     List[float]
         The scaled intensities.
     """
-    return (
-        1000 * (intensity - min(intensity)) / (max(intensity) - min(intensity))
-    )
+    return intensity * 1000 / np.max(intensity)